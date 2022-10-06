e192ab18dc1bfd7cb0876335b4a45a5d5f50d205
abdelbaki1 <chriguiabdelbaki@gmail.com>
2022-10-06 14:34:06 +0100

    django-test

---
+++ users/auth.py
@@ -0,0 +1,43 @@
+import jwt
+import datetime
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication
+
+
+def generate_access_token(user):
+    payload = {
+        'user_id': user.id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+        'iat': datetime.datetime.utcnow()
+    }
+    encoded_jwt=jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    token_object=token(user=user,user_token=encoded_jwt)
+    token_object.save()
+
+    return encoded_jwt.decode('utf-8')
+
+
+class JWTAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        if 'jwt' in request.COOKIES:
+            token = request.COOKIES.get('jwt')
+        # else:
+        #     #retreeve from the header
+        #     token=token.object.
+        if not token:
+            return None
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('unauthenticated')
+
+        user = get_user_model().objects.filter(id=payload['user_id']).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+
+        return (user, None)
